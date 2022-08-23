d6f95ae777822381e69d1db15646f196a4af9495
Jose Briones Romero <jose.briones.r@hotmail.com>
2022-08-23 15:41:00 -0500

    add project

---
+++ TVmaze/TVmazeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TVmazeApp.swift
+//  TVmaze
+//
+//  Created by José Briones Romero on 23/8/22.
+//
+
+import SwiftUI
+
+@main
+struct TVmazeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
